07c8cf3da6a6f21c88b4d32e81ba025ced7adff3
Macbook <macbook@pc-26.home>
2021-09-01 14:21:30 +0200

    emacs config

---
+++ custom.el
@@ -0,0 +1,14 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   (quote
+    (forge github-review theme-magic epoch-view log4j-mode pretty-mode restclient key-chord dumb-jump diminish htmlize sudo-edit web-mode company-shell slime-company slime python-pytest company-jedi company-irony company-c-headers flycheck-clang-analyzer yasnippet-snippets popup-kill-ring zzz-to-char smart-hungry-delete expand-region beacon mark-multiple helm-projectile swiper switch-window ivy shell-pop symon fancy-battery mood-line dashboard treemacs-magit treemacs-icons-dired balanced-windows color-theme-approximate kaolin-themes github-theme yaml-mode rainbow-mode npm-mode nginx-mode nasm-mode markdown-mode jinja2-mode groovy-mode elpy dotenv-mode docker demangle-mode csv-mode apache-mode angular-mode anaconda-mode atom-one-dark-theme spacemacs-theme doom-themes gruvbox-theme zenburn-theme material-theme monokai-theme zerodark-theme badger-theme ztree which-key use-package-ensure-system-package treemacs-projectile treemacs sphinx-doc rainbow-delimiters projectile phpunit paradox org-bullets magit-gitflow magit lv helm-swoop helm git-messenger git-gutter ggtags fontawesome fill-column-indicator emojify doom-modeline dtrt-indent desktop+ anzu all-the-icons ace-window ac-php ac-html-csswatcher ac-html ac-etags 0blayout ansible-vault ansible-doc ansible py-autopep8 flycheck better-defaults use-package))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
